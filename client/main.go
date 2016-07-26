7a481b3327694d7431dd41e7096e02c9c1140b7d
Pieter Joost van de Sande <pj@born2code.net>
2016-07-26 19:22:18 +0200

    add ping command to cli

--- client/main.go
+++ client/main.go
@@ -18,6 +18,40 @@ func main() {
 	app := cli.NewApp()
 	app.Commands = []cli.Command{
 		{
+			Name:  "ping",
+			Usage: "ping host",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "host",
+					Value:  "localhost:6300",
+					Usage:  "the address of the host",
+					EnvVar: "STRAND_HOST",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				host := c.String("host")
+
+				// Set up a connection to the server.
+				conn, err := grpc.Dial(host, grpc.WithInsecure())
+				if err != nil {
+					log.Fatalf("failed to connect: %v", err)
+				}
+				defer conn.Close()
+
+				client := api.NewStrandClient(conn)
+				elapsed := stopwatch.Time(func() {
+					_, err = client.Ping(context.Background(), &api.PingRequest{})
+				})
+
+				if err != nil {
+					fmt.Printf("request failed: %v", err)
+				}
+
+				fmt.Printf("elapsed: %v", elapsed)
+				return nil
+			},
+		},
+		{
 			Name:    "publish",
 			Aliases: []string{"p"},
 			Usage:   "publish messages to topic",

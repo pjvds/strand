f8c91f4c9608eaa388806cfc751f2bd9a4d7e12c
Pieter Joost van de Sande <pj@born2code.net>
2016-08-03 19:24:05 +0200

    move server to own package

--- main.go
+++ main.go
@@ -5,65 +5,17 @@ import (
 	"net"
 	"time"
 
-	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	"github.com/pjvds/tidy"
 
 	"github.com/pjvds/strand/api"
-	"github.com/pjvds/strand/message"
-	"github.com/pjvds/strand/stream"
+	"github.com/pjvds/strand/server"
 )
 
 var log = tidy.Configure().
 	LogFromLevel(tidy.DEBUG).To(tidy.Console).
 	MustBuild()
-
-type Server struct {
-	streams *stream.Map
-}
-
-func NewServer(directory string) *Server {
-	return &Server{
-		streams: stream.NewMap(stream.Directory(directory).OpenOrCreateStream),
-	}
-}
-
-func (this *Server) Append(ctx context.Context, request *api.AppendRequest) (*api.AppendResponse, error) {
-	id := stream.Id(request.Stream)
-	if log.IsDebug() {
-		log.With("stream_id", id).Debug("handling append request")
-	}
-
-	s, err := this.streams.Get(id)
-	if err != nil {
-		if log.IsInfo() {
-			log.With("stream_id", id).WithError(err).Info("failed to get stream")
-		}
-		return nil, err
-	}
-
-	set, err := message.NewUnalignedSet(request.Messages)
-	if err != nil {
-		if log.IsDebug() {
-			log.WithError(err).Debug("failed to parse message set")
-		}
-		return nil, err
-	}
-
-	_, err = s.Append(set)
-	if err != nil {
-		return nil, err
-	}
-
-	return &api.AppendResponse{
-		Ok: true,
-	}, nil
-}
-
-func (this *Server) Ping(context.Context, *api.PingRequest) (*api.PingResponse, error) {
-	return &api.PingResponse{}, nil
-}
 
 func main() {
 	network := "tcp"
@@ -74,14 +26,14 @@ func main() {
 		return
 	}
 
-	server := grpc.NewServer()
-	api.RegisterStrandServer(server, NewServer("/tmp"))
+	grpcServer := grpc.NewServer()
+	api.RegisterStrandServer(grpcServer, server.NewServer("/tmp"))
 
 	log.Withs(tidy.Fields{
 		"network": network,
 		"address": address}).Info("listening")
 
-	if err := server.Serve(listener); err != nil {
+	if err := grpcServer.Serve(listener); err != nil {
 		log.WithError(err).Fatal("grpc server failed")
 	}
 }
---
+++ server/server.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"github.com/pjvds/strand/api"
+	"github.com/pjvds/strand/message"
+	"github.com/pjvds/strand/stream"
+	"github.com/pjvds/tidy"
+	"golang.org/x/net/context"
+)
+
+var log = tidy.Configure().
+	LogFromLevel(tidy.DEBUG).To(tidy.Console).
+	MustBuild()
+
+type Server struct {
+	streams *stream.Map
+}
+
+func NewServer(directory string) *Server {
+	return &Server{
+		streams: stream.NewMap(stream.Directory(directory).OpenOrCreateStream),
+	}
+}
+
+func (this *Server) Append(ctx context.Context, request *api.AppendRequest) (*api.AppendResponse, error) {
+	id := stream.Id(request.Stream)
+	if log.IsDebug() {
+		log.With("stream_id", id).Debug("handling append request")
+	}
+
+	s, err := this.streams.Get(id)
+	if err != nil {
+		if log.IsInfo() {
+			log.With("stream_id", id).WithError(err).Info("failed to get stream")
+		}
+		return nil, err
+	}
+
+	set, err := message.NewUnalignedSet(request.Messages)
+	if err != nil {
+		if log.IsDebug() {
+			log.WithError(err).Debug("failed to parse message set")
+		}
+		return nil, err
+	}
+
+	_, err = s.Append(set)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.AppendResponse{
+		Ok: true,
+	}, nil
+}
+
+func (this *Server) Ping(context.Context, *api.PingRequest) (*api.PingResponse, error) {
+	return &api.PingResponse{}, nil
+}

d867f02b06fe61b8ec984190a3b3cddc483ad69c
Pieter Joost van de Sande <pj@born2code.net>
2016-08-03 09:12:10 +0200

    add logging to append request handling

--- main.go
+++ main.go
@@ -31,14 +31,23 @@ func NewServer(directory string) *Server {
 
 func (this *Server) Append(ctx context.Context, request *api.AppendRequest) (*api.AppendResponse, error) {
 	id := stream.Id(request.Stream)
-	s, err := this.streams.Get(id)
+	if log.IsDebug() {
+		log.With("stream_id", id).Debug("handling append request")
+	}
 
+	s, err := this.streams.Get(id)
 	if err != nil {
+		if log.IsInfo() {
+			log.With("stream_id", id).WithError(err).Info("failed to get stream")
+		}
 		return nil, err
 	}
 
 	set, err := message.NewUnalignedSet(request.Messages)
 	if err != nil {
+		if log.IsDebug() {
+			log.WithError(err).Debug("failed to parse message set")
+		}
 		return nil, err
 	}
 
@@ -51,6 +60,7 @@ func (this *Server) Append(ctx context.Context, request *api.AppendRequest) (*ap
 		Ok: true,
 	}, nil
 }
+
 func (this *Server) Ping(context.Context, *api.PingRequest) (*api.PingResponse, error) {
 	return &api.PingResponse{}, nil
 }
